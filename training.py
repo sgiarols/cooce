ec2d8b2db5d51c19c7300c9ffe0597968037760c
Sara <sara@Saras-MBP-2.station>
2024-11-12 00:16:24 +0100

    updated ADH2 costs

--- training.py
+++ training.py
@@ -104,7 +104,7 @@ class Assets:
         #assumption on scaling factor
         capADH2 = self.assets.loc[self.assets.index=="AD"].values*CO2split
 
-        capital_costs.loc[capital_costs.index=="ADH2"] += 0.35 * (capADH2)**0.7 
+        capital_costs.loc[capital_costs.index=="ADH2"] += 0.06 * (capADH2)**0.7 
 
         capital_costs.columns = ["capital_costs"]
         return capital_costs
